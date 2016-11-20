0c0b42c8a64f5672dd75d3e1786be1e5cbc29426
JoshuaNS <Joshns@outlook.com>
2016-11-20 17:26:32 -0500

    Added skeleton of FloodingAlgorithm class.

---
+++ src/FloodingAlgorithm.java
@@ -0,0 +1,35 @@
+
+public class FloodingAlgorithm extends RoutingAlgorithm {
+
+	@Override
+	public void simulateStep() {
+		if (!isSimulating()) {
+			return;
+		}
+		
+		for (Node n : getGraph().getNodes()) {
+			for (Packet p : n.getPackets()) {
+				if (!p.isTransfered()) {
+					
+					// Packet has reached it's destination, so remove it.
+					if (n.equals(p.getDestination())) {
+						n.removePacket(p);
+						// Need to remove all copies that exist of this packet elsewhere.
+					}
+					
+					n.removePacket(p);
+					for (Node conn : n.getConnections()) {
+						// Need to make the node to send to is not the node the packet came from.
+						
+						conn.addPacket(p);
+						p.setTransfered(true);
+					}
+				}
+			}
+		}
+		
+		resetTransfered();
+
+	}
+
+}
