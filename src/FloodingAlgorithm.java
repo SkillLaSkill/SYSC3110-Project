3763e5f8c1f9046170c13ce292786ca8efad852c
thomas7carriere <thomas7carriere@gmail.com>
2016-12-02 12:13:49 -0500

    Updated FloodAlg
    Need to update Packet and Node:
    //add time to live in packet (starting at #, decreasing to 0)
    //add time to live getter(getTimeToLive) Packet
    //add packetsSeen array in Node
    //add hasSeen method in Node
    //add addHasSeen method in Node

--- src/FloodingAlgorithm.java
+++ src/FloodingAlgorithm.java
@@ -1,86 +1,82 @@
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Runs the flood search algorithm which sends a packet to all the nodes connections. 
+ * Runs the random search algorithm which keeps going to a random node until
+ * it reaches the destination.
  * 
  * @author Team GetterDone
  */
-public class FloodingAlgorithm extends RoutingAlgorithm {
-	private Set<Packet> packets = new HashSet<Packet>();
+public class FloodingAlgorithm extends RoutingAlgorithm { 
 	
+	private Set<Packet> packets = new HashSet<Packet>();
 	/**
-	 * Steps through the simulation using the flood method
+	 * Steps through the simulation using the random method
 	 */
 	@Override
 	public void simulateStep() {
 		int packetsSentThisStep = 0;
 		int packetsFinishedThisStep = 0;
 		
+		for (int i = 0; i < getGraph().getNodes().size(); i++){
+			Node n = getGraph().getNodes().get(i);
+			
+			for (int j = 0; j < n.getPackets().size(); j++) {
+				Packet p = n.getPackets().get(j);
+				
+				for( int k = 0; k < n.getConnections.size(); k++) {
+					Node con = n.getConnections(k);
+					
+					if(!con.hasSeen(p)){ //checks if connection has seen the packet
+						con.addPacket(p); 
+						con.seen(p); //adds Packet to hasSeen list of Connection
+						p.incrementHops();
+						packetsSentThisStep++;
+					}
+					
+					// Packet has reached it's destination, so add its hops counter to the totalHops then remove it.
+					if (con.equals(p.getDestination())) {
+						packetsFinishedThisStep++;
+						con.removePacket(p);
+						return;
+					}
+				}
+			}
+		}
+		//return metrics to the metric class
+		getMetric().addHops(packetsSentThisStep);
+		getMetric().addCompleteTransfers(packetsFinishedThisStep);
+		
 		//creates a set of all packets in the graph
 		for(int i = 0; i < getGraph().getNodes().size(); i++) {
 			Node n = getGraph().getNodes().get(i);
+			
 			for (int j = 0; j < n.getPackets().size(); j++) {
 				Packet p = n.getPackets().get(j);
 				packets.add(p);
 			}
 		}
+		//decrement the time to live of each packet
 		Object[] packetsArray = packets.toArray();
-		//need a Time To Live counter
 		for(int i = 0; i < packetsArray.length; i++) {
 			Packet p = (Packet)packetsArray[i];
+			p.decrementTimeToLive();
 			
-			for(int j = 0; j < getGraph().getNodes().size(); j++) {
-				Node n = getGraph().getNodes().get(j);
-				if(n.countainsPacket(p)) {
-					
-					int seenCounter = 0;
-					for(int k = 0; k < n.getConnections().size() ; k++) {
-						Node con = n.getConnections().get(k);
-						
-						if(!con.hasSeenPacket(p)) {
-							p.incrementHops();
-							packetsSentThisStep++;
-							
-							//node has reach destination
-							if(con.equals(p.getDestination())) {
-								n.addSeenPacket(p);
-								n.removePacket(p);	
-								
-								packetsFinishedThisStep++;
-							}
-							else {
-								con.addPacket(p);
-								con.addSeenPacket(p);
-							}
-						} else {
-							seenCounter++;
-						}
-						// All connections already visited.
-						if (seenCounter == n.getConnections().size() - 1) {
-							n.removePacket(p);
-						}
-					}					
-				}		
-			}
-			getMetric().addHops(packetsSentThisStep);
-			getMetric().addCompleteTransfers(packetsFinishedThisStep);
-		}
-		//checks if any of the packets have expired and need to be removed
-		for(int i = 0; i < packetsArray.length; i++) {
-			Packet p = (Packet)packetsArray[i];
-			
-			if(p.getHops() > 8) {
-			
+			//if packet has expired, remove all instances of of it from graph
+			if(p.getTimeToLive <= 0) { //if packet has expired, remove all instances of of it from graph
 				for(int j = 0; j < getGraph().getNodes().size(); j++) {
 					Node n = getGraph().getNodes().get(j);
-					if(n.countainsPacket((Packet) p)) {
-						n.removePacket((Packet) p);
-					}
+					if(n.hasPacket(p)) j.removePacket(p);
 				}
+		
 			}
 		}
 	}
-	
-	
 }
+//add time to live in packet (starting at number, decreasing to 0)
+//add time to live getter(getTimeToLive)
+//add packetsSeen array in Node
+//add hasSeen method in Node
+//add addHasSeen method in Node
